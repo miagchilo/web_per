35b87cb7acf1faae85f9a09324b69d7088f68817
Stan Miagchilo <stasss95@mail.ru>
2024-03-29 11:40:31 +0100

    index changhed

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,34 @@
-import { defineConfig } from 'vite';
-import imageminPlugin from 'vite-plugin-imagemin';
+import { defineConfig } from "vite";
+import imageminPlugin from "vite-plugin-imagemin";
+import cssnanoPlugin from "cssnano";
+import { visualizer } from "rollup-plugin-visualizer";
+
 
 export default defineConfig({
-  plugins: [
-    imageminPlugin(), 
-  ],
-  base: './',
+  plugins: [imageminPlugin(),
+    visualizer({
+    minify: true,
+    open: true,
+    gzipSize: true,
+    brotliSize: true,
+  }), cssnanoPlugin()],
+  base: "./",
 
-  
   server: {
-   
     port: 3000,
-    
+
     open: true,
   },
 
-  
   build: {
-    
-    outDir: 'dist',
-    
+    outDir: "dist",
+
     minify: true,
-    
+
     chunkSizeWarningLimit: 1000,
-   
   },
   rollupOptions: {
-    input: './index.html',
+    input: "./index.html",
   },
 
   // Define any additional plugins or adjustments here
